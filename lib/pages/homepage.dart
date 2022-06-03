978c4e016190491b8cc15e7e733b2c1e535e8b10
UTKARSH SINGH <us9766134@gmail.com>
2022-06-04 01:40:53 +0530

    day1

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,19 @@
+import "package:flutter/material.dart";
+class Homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final int da=30;
+    final String name ="boss";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Meta data"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("this is $da app by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
