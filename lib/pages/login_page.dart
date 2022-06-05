99bd50b585461ddc79897d9d06e7904dc36dab85
UTKARSH SINGH <us9766134@gmail.com>
2022-06-06 01:39:57 +0530

    day3

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/utils/routes.dart';
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  String name="";
+  bool changebu  = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -10,7 +17,7 @@ class Login extends StatelessWidget {
           children: [
             Image.asset("assets/login_image.png",fit:BoxFit.fill,height: 400,
             ),
-            Text("WELCOME",style: TextStyle(
+            Text("WELCOME $name",style: TextStyle(
               fontSize: 22,fontWeight: FontWeight.bold),
             ),
             SizedBox(
@@ -25,6 +32,10 @@ class Login extends StatelessWidget {
                     hintText: "Enter username",
                     labelText: "Username",
                   ),
+                  onChanged: (value){
+                    name = value;
+                    setState(() {});
+                  },
                 ),
                 TextFormField(
                   obscureText: true,
@@ -36,13 +47,42 @@ class Login extends StatelessWidget {
                   SizedBox(
                     height: 50.0,
                   ),
-                  ElevatedButton(
+                  InkWell(
+                    onTap: ()  async {
+                      setState((){
+                        changebu =true;
+                      });
+                      await Future.delayed(Duration(seconds: 1));
+                      Navigator.pushNamed(context, myroutes.homeroute);
+                    },
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      height: 40,
+                      width: changebu?40:80,
+                      alignment: Alignment.center,
+                      child: changebu?Icon(
+                        Icons.done,
+                        color: Colors.white ) : Text(
+                        "Login",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        borderRadius: BorderRadius.circular(8),
+                      )
+                    ),
+                  )
+                  /*ElevatedButton(
                     child: Text("login"),
                     style: TextButton.styleFrom(minimumSize: Size(100, 50)),
                     onPressed: () {
                   Navigator.pushNamed(context, myroutes.homeroute);
                     },
-                    ),
+                    ),*/
     ],
     ),
     ),
